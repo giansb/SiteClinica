33f873f376ee10fa71d3d0d238ad045578d428f0
Gian <gianbraga1802@gmail.com>
2023-05-20 08:45:10 -0300

    new

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import PaginaEspecialidades from './componentes/PaginaEspecialidades';
 import PaginaHome from './componentes/PaginaHome';
 import Rotas from './componentes/Rotas';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path='/SiteClinica' element={<PaginaHome/>}></Route>
           <Route path='/SiteClinica/especialidades' element={<PaginaEspecialidades/>}></Route>
         </Routes>      
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
