a61a79c3b4f71b6f001dec53467c66aaa3f7e30d
Gian <gianbraga1802@gmail.com>
2023-05-20 10:24:36 -0300

    new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import PaginaEspecialidades from './componentes/PaginaEspecialidades';
 import PaginaHome from './componentes/PaginaHome';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , Switch} from "react-router-dom";
 
 
 
